dab78f311a5772d4815762e98960fea6c92dbf5c
Philip Wales <me@ptwales.net>
2019-02-15 19:57:49 -0600

    Merge MapOptions with Map and remove runOptions
    Added a type alias for MapOptionsR

--- src/Map.purs
+++ src/Map.purs
@@ -1,21 +1,30 @@
-module GMaps.Map (Map (), gMap, panTo) where
+module GMaps.Map 
+  ( Map
+  , MapOptions
+  , gMap
+  , panTo
+  ) where
 
 import Prelude (Unit)
 import Effect (Effect)
 import Web.DOM (Element)
 import GMaps.LatLng (LatLng)
-import GMaps.MapOptions (MapOptions, runMapOptions)
 import Data.Function.Uncurried (Fn2, runFn2)
+
+type MapOptionsR =
+  { zoom :: Number
+  , center :: LatLng
+  , mapTypeId :: String
+  }
+
+newtype MapOptions = MapOptions MapOptionsR
 
 foreign import data Map :: Type
 
-foreign import gMapImpl :: Fn2 Element { zoom :: Number, center :: LatLng, mapTypeId :: String } (Effect Map)
-
-gMapFFI :: Element -> { zoom :: Number, center :: LatLng, mapTypeId :: String } -> Effect Map
-gMapFFI = runFn2 gMapImpl
+foreign import gMapImpl :: Fn2 Element MapOptionsR (Effect Map)
 
 gMap :: Element -> MapOptions -> Effect Map
-gMap e m = gMapFFI e (runMapOptions m)
+gMap element (MapOptions options) = runFn2 gMapImpl element options
 
 foreign import panToImpl :: Fn2 Map LatLng (Effect Unit)
 
