b6f277501293391cd8eb7289bb66fe590f8bc4de
Philip Wales <me@ptwales.net>
2019-02-15 20:39:16 -0600

    Add all options to MarkerOptions

--- src/Marker.purs
+++ src/Marker.purs
@@ -9,7 +9,7 @@ module GMaps.Marker
 
 import Prelude (Unit, (<<<))
 import Data.Function.Uncurried (Fn1, runFn1, Fn2, runFn2)
-import Data.Maybe (Maybe, fromMaybe)
+import Data.Maybe (Maybe(Nothing), fromMaybe)
 import Effect (Effect)
 import GMaps.LatLng (LatLng)
 import GMaps.Map (Map)
@@ -17,15 +17,56 @@ import GMaps.Map (Map)
 type MarkerOptionsR =
   { position :: LatLng
   , map :: Map
-  , title :: String
+  --, anchorPoint :: Point
+  --, animation :: Animation
+  , clickable :: Boolean
+  , crossOnDrag :: Boolean
+  --, cursor :: String
+  , draggable :: Boolean
   , icon :: String
+  , label :: Char
+  , optimized :: Boolean
+  --, shape :: MarkerShape
+  , title :: String
+  , visible :: Boolean
+  , zIndex :: Number
   }
 
 newtype MarkerOptions = MarkerOptions
   { position :: LatLng
-  , map :: Map
-  , title :: String
+  , map :: Maybe Map
+  --, anchorPoint :: Maybe Point
+  --, animation :: Maybe Animation
+  , clickable :: Boolean
+  , crossOnDrag :: Boolean
+  --, cursor :: Maybe String
+  , draggable :: Boolean
   , icon :: Maybe String
+  , label :: Maybe Char
+  , optimized :: Boolean
+  --, shape :: Maybe MarkerShape
+  , title :: Maybe String
+  , visible :: Boolean
+  , zIndex :: Maybe Number
+  }
+
+defMarkerOptions :: LatLng -> MarkerOptions
+defMarkerOptions position = MarkerOptions
+  { position
+  , map: Nothing
+  --, anchorPoint: Nothing
+  --, animation: Nothing
+  , clickable: true
+  , crossOnDrag: true
+  --, cursor: Nothing
+  , draggable: false
+  , icon: Nothing
+  , label: Nothing
+  , optimized: true
+  --, shape: Nothing
+  , title: Nothing
+  , visible: true
+  , zIndex: Nothing
   }
 
 -- GMaps either wants a marker icon or undefined.
@@ -37,9 +78,18 @@ foreign import newMarkerImpl :: Fn1 MarkerOptionsR (Effect Marker)
 
 newMarker :: MarkerOptions -> Effect Marker
 newMarker = runFn1 newMarkerImpl <<< runMarkerOptions
-  where runMaybe = fromMaybe undefined
+  where runMaybe :: forall a. Maybe a -> a
+        runMaybe = fromMaybe undefined
         runMarkerOptions (MarkerOptions options) = options 
-          { icon = runMaybe options.icon
+          { map = runMaybe options.map
+          --, anchorPoint = runMAybe options.anchorPoint
+          --, animation = runMAybe options.animation
+          --, cursor = runMAybe options.cursor
+          , icon = runMaybe options.icon
+          , label = runMaybe options.label
+          --, shape = runMaybe options.shape
+          , title = runMaybe options.title
+          , zIndex = runMaybe options.zIndex
           }
 
 foreign import removeMarkerImpl :: Fn1 Marker (Effect Marker)
